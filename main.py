b2146df6571e5a1562d8756201e43ca5e45996ee
Coco Jambo <redaprobal@gmaill.com>
2024-01-08 23:41:57 -0500

    This is the base code, opens the camera of your computer lez gooo

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("test")
+import cv2 
+
+#Loading camera
+cap = cv2.VideoCapture(0)
+while True:
+    success, frame = cap.read()
+    
+    
+    if success:
+       cv2.imshow("Funky Cam", frame)
+    
+    # Close and break the loop after pressing "x" key
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
+
+# close the already opened camera
+cap.release()
+# close the window and de-allocate any associated memory usage
+cv2.destroyAllWindows()
