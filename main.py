3a6f51e82aec3d3b33eef46fcf9b4d812cce1103
Coco Jambo <redaprobal@gmaill.com>
2024-01-09 00:28:27 -0500

    Resized the camera to 1080p

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+#Author: Coco-Jambo
+#Start date: January 8th 2024
+#Project name: FunkyCam
+
+#importing the opencv library
 import cv2 
 
-#Loading camera
+#Loading camera, the int is the id of the device to open if there are multiple
 cap = cv2.VideoCapture(0)
-while True:
+
+#scaling the camera to 1080p
+cap.set(cv2.CAP_PROP_FRAME_WIDTH , 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 1080)
+
+#Main loop
+while  True:
+    #retreiving camera data
     success, frame = cap.read()
+
+   
     
-    
+    #If the video frame has been captured open the camera window
     if success:
+       
        cv2.imshow("Funky Cam", frame)
+
     
-    # Close and break the loop after pressing "x" key
-    if cv2.waitKey(1) & 0xFF == ord('x'):
+    # Close and break the loop after pressing "x" key, or after clicking x on the window
+    if (cv2.waitKey(1) and 0xFF == ord('x')):
         break
+
 
 # close the already opened camera
 cap.release()
