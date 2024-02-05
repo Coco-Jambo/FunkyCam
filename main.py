f01e52effe6d21bf1de3940657d219430be32d65
Réda <92837183+Coco-Jambo@users.noreply.github.com>
2024-02-05 01:58:48 -0500

    added somethings (numpy) I still need to look into how to add an image, it puts a rectangle around your face but I wanna put an image. Maybe I need to detect the eyes to get the tilt, but I want to try to use the face frist then I'll see if I need the app to detect the eyes

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 #importing the libraries
 import cv2
 import sys
+import numpy as np
 
 #creating a face cascade
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -17,7 +18,7 @@ faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 basic_expression = cv2.imread('assets/SMILE PNG.png', cv2.IMREAD_UNCHANGED)
 
 #the ratio is used when resizing the image (width/height)
-ratio = basic_expression[1]/basic_expression[0]
+ratio = basic_expression.shape[1]/basic_expression.shape[0]
 
 #Loading camera, the int is the id of the device to open if there are multiple
 cap = cv2.VideoCapture(0)
@@ -26,46 +27,47 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH , 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 720)
 
+
+#retreiving camera data
+success, frame = cap.read()
+
 #Main loop
-while  True:
-    #retreiving camera data
-    success, frame = cap.read()
+#while the video frame has been captured open the camera window
+while  success:
+    #This is what deals with the face detection stuff
+    #First this converts the input from rgb to grayscale and puts the image in gray
+    #gray images are better for face detection sicne they simplify the image and reduce computational load
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #If the video frame has been captured open the camera window
-    if success:
-        
-        #This is what deals with the face detection stuff
-        #First this converts the input from rgb to grayscale and puts the image in gray
-        #gray images are better for face detection sicne they simplify the image and reduce computational load
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    #we use the method detectMultiscale to do face detection
+    # - gray is the parameter holding the current frame (the input image)
+    # - scaleFactor is the parameter that compensates for when faces appear smaller when they are far
+    #       or big when they are close, it scales the image to 1.1 to help detect the faces at different
+    #       distances
+    # - minNeighbors is the parameter that specifies how many neighbors (other overlapping rectangles) minimum to be a valid face,
+    #       to reduce the amount of false positives the algorithm may detect. For example it can detect many faces inside a face and 
+    #       draw multiple rectangles inside a face that overlap, minNeighbors filters out these detections and refines 
+    #       the result. If the count of overlapping rectangles is greater than or equal to it, then it is a valid face,
+    #       otherwise it is a false positive. It's like a sensitivity parameter
+    # - minSize is a tuple that specifies the minimum size of a face (in pixels)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-        
-        #we use the method detectMultiscale to do face detection
-        # - gray is the parameter holding the current frame (the input image)
-        # - scaleFactor is the parameter that compensates for when faces appear smaller when they are far
-        #       or big when they are close, it scales the image to 1.1 to help detect the faces at different
-        #       distances
-        # - minNeighbors is the parameter that specifies how many neighbors (other overlapping rectangles) minimum to be a valid face,
-        #       to reduce the amount of false positives the algorithm may detect. For example it can detect many faces inside a face and 
-        #       draw multiple rectangles inside a face that overlap, minNeighbors filters out these detections and refines 
-        #       the result. If the count of overlapping rectangles is greater than or equal to it, then it is a valid face,
-        #       otherwise it is a false positive. It's like a sensitivity parameter
-        # - minSize is a tuple that specifies the minimum size of a face (in pixels)
-        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    # Draw a rectangle around the faces detected
+    # faces is the list of detected faces
+    # (x,y) is the top-left corner
+    # frame is the input image where the rectangles will be drawn
+    # (x+w, y+h) bottom-right corner (w is width and h is height)
+    # (0, 255, 0) is the color of the rectangle
+    # 2 is the thickness of the border
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    #displaying frame
+    cv2.imshow("Funky Cam", frame)
 
-        # Draw a rectangle around the faces detected
-        # faces is the list of detected faces
-        # (x,y) is the top-left corner
-        # frame is the input image where the rectangles will be drawn
-        # (x+w, y+h) bottom-right corner (w is width and h is height)
-        # (0, 255, 0) is the color of the rectangle
-        # 2 is the thickness of the border
-        for (x, y, w, h) in faces:
-            cv2.add
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        
-        #displaying frame
-        cv2.imshow("Funky Cam", frame)
+    #get next frame
+    success, frame  = cap.read()
 
     # Close and break the loop after pressing "x" key, or after clicking x on the window
     if (cv2.waitKey(1) & 0xFF == ord('x')):
